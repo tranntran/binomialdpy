c340a7671a54de6661931336d98fc4e6938407d5
Tran Tran <ttran2401@gmail.com>
2022-11-14 02:43:50 -0500

    Add code for two sided confidence interval

--- binomialdpy/ci.py
+++ binomialdpy/ci.py
@@ -51,3 +51,58 @@ def upper(alpha, sample, n, b, q):
         tmp1 = minimize(ci_obj, 0.5, method='L-BFGS-B', bounds=((0,1),))
         ans.append(tmp1.x[0])
     return alpha
+
+
+def two_side(alpha, sample, n, b, q):
+    ans = []
+    for s in sample:
+        mle = s/n
+        mle = max(min([mle, 1]), 0)
+
+        def ci_obj2(p):
+            tmp = pvalue.two_side(sample = [s], p = p, n = n, b = b, q = q)
+            return tmp[0] - alpha
+        
+        if mle > 0:
+            tmp = minimize(ci_obj, mle/2, method='L-BFGS-B', bounds=((0, mle),))
+            L = tmp.x
+        else:
+            L = 0
+        
+        if mle < 1:
+            tmp = minimize(ci_obj, (1-mle)/2, method='L-BFGS-B', bounds=((mle, 1),))
+            U = tmp.x
+        else:
+            U = 1
+        ans.append((L[0], U[0]))
+    return ans      
+
+
+CITwoSide <- function(alpha , Z, size, b, q){
+  mle = Z/size
+  mle = base::max(base::min(mle, 1), 0)
+  CIobj2 = function(theta, alpha, Z, size, b, q){
+    return((pvalTwoSide(Z = Z, theta = theta, size = size, b = b, q = q) - alpha)^2)
+  }
+
+  if(mle > 0){
+    L = stats::optim(par = mle/2, fn = CIobj2,
+                     alpha = alpha, Z = Z, size = size, b = b, q = q,
+                     method = "Brent", lower = 0, upper = mle)
+    L = L$par
+  } else {
+    L = 0
+  }
+
+  if(mle < 1){
+    U = stats::optim(par = (1-mle)/2, fn = CIobj2,
+                     alpha = alpha, Z = Z, size = size, b = b, q = q,
+                     method = "Brent", lower = mle, upper = 1)
+    U = U$par
+  } else {
+    U = 1
+  }
+
+  CI = c(L, U)
+  return(CI)
+}
